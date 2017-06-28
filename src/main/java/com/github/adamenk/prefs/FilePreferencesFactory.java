1e36414b2613c6685a2bbd557ec2543ea3b84809
adamenk <mnadamenko@gmail.com>
2017-06-28 15:45:17 +0300

    added Java Preferences API extension to work with file as the properties storage

---
+++ src/main/java/com/github/adamenk/prefs/FilePreferencesFactory.java
@@ -0,0 +1,49 @@
+package com.github.adamenk.prefs;
+
+import java.io.File;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+import java.util.prefs.PreferencesFactory;
+
+/**
+ * PreferencesFactory implementation that stores the preferences in a user-defined file. To use it,
+ * set the system property <tt>java.util.prefs.PreferencesFactory</tt> to
+ * <tt>com.github.adamenk.prefs.FilePreferencesFactory</tt>
+ * <p/>
+ * The file defaults to [user.home]/.fileprefs, but may be overridden with the system property
+ * <tt>com.github.adamenk.prefs.FilePreferencesFactory.file</tt>
+ */
+public class FilePreferencesFactory implements PreferencesFactory {
+    private static final Logger log = Logger.getLogger(FilePreferencesFactory.class.getName());
+
+    private Preferences rootPreferences;
+    public static final String SYSTEM_PROPERTY_FILE =
+            "com.github.adamenk.prefs.FilePreferencesFactory.file";
+
+    public Preferences systemRoot() {
+        return userRoot();
+    }
+
+    public Preferences userRoot() {
+        if (rootPreferences == null) {
+            log.finer("Instantiating root preferences");
+
+            rootPreferences = new FilePreferences(null, "");
+        }
+        return rootPreferences;
+    }
+
+    private static File preferencesFile;
+
+    public static File getPreferencesFile() {
+        if (preferencesFile == null) {
+            String prefsFile = System.getProperty(SYSTEM_PROPERTY_FILE);
+            if (prefsFile == null || prefsFile.length() == 0) {
+                prefsFile = System.getProperty("user.home") + File.separator + ".fileprefs";
+            }
+            preferencesFile = new File(prefsFile).getAbsoluteFile();
+            log.finer("Preferences file is " + preferencesFile);
+        }
+        return preferencesFile;
+    }
+}
---
+++ src/test/java/com/github/adamenk/prefs/FilePreferencesTest.java
@@ -0,0 +1,25 @@
+package com.github.adamenk.prefs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.prefs.Preferences;
+
+import static com.github.adamenk.prefs.FilePreferencesFactory.SYSTEM_PROPERTY_FILE;
+
+public class FilePreferencesTest {
+
+    @Test
+    public void test1() throws Exception {
+        System.setProperty("java.util.prefs.PreferencesFactory", FilePreferencesFactory.class.getName());
+        System.setProperty(SYSTEM_PROPERTY_FILE, "testprefs.txt");
+
+        Preferences p = Preferences.userNodeForPackage(this.getClass());
+
+        p.putBoolean("boolean.property", true);
+        p.put("string.property", "custom property value");
+
+        Assert.assertEquals(p.get("boolean.property",null), "true");
+        Assert.assertEquals(p.get("string.property",null), "custom property value");
+    }
+}
